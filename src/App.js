7b1d83bda931aed04706aa56d3916709215fc0f9
mohammedaabouche <mo.aabouche2002@gmail.com>
2024-10-20 10:05:46 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Home from './pages/Home';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-      </Routes>
-    </Router>
-
+    <Home/>
   );
 }
 
